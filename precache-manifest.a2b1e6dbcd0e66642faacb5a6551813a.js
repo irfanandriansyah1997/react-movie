d98f66dc8053ffeac758bbf7a7b75afb0db14be2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-17 16:57:25 +0000

    Deploy irfanandriansyah1997/react-movie to github.com/irfanandriansyah1997/react-movie.git:gh-pages

---
+++ precache-manifest.a2b1e6dbcd0e66642faacb5a6551813a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39199d0797d57f09940c22b63f410ae0",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "cb193a2d5876fe99fcdf",
+    "url": "/react-movie/static/css/main.af5f5102.chunk.css"
+  },
+  {
+    "revision": "923ed7ee7af6571ed973",
+    "url": "/react-movie/static/js/2.0acc09af.chunk.js"
+  },
+  {
+    "revision": "1a3fec05e970fac949db0e809c2f1ac1",
+    "url": "/react-movie/static/js/2.0acc09af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb193a2d5876fe99fcdf",
+    "url": "/react-movie/static/js/main.2b2d6cec.chunk.js"
+  },
+  {
+    "revision": "5fd70710d2feb5432c6e",
+    "url": "/react-movie/static/js/runtime-main.1567f7ed.js"
+  }
+]);
